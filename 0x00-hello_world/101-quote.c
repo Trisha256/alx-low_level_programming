34f98cfb1d0460f23a0af642d70dd458f3775c5e
Patricia Kemirembe <kemirembe2patricia@gmail.com>
2023-10-05 10:45:57 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <unistd.h>
+/**
+ * main - a program that prints a line to a standard error
+ * Return: 1
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n" 59);
+	return (1);
+}

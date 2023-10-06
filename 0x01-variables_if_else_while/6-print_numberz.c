d439e7c4ff0a88972f34fe0ba4d57d06c57ce901
Patricia Kemirembe <kemirembe2patricia@gmail.com>
2023-10-06 10:33:10 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -10,11 +10,11 @@ int main(void)
 {
 	int a;
 
-        for (a = 0; a < 10; a++)
-        {
-        putchar(a + '0');
-        }
-        putchar('\n');
+	for (a = 0; a < 10; a++)
+	{
+	putchar(a + '0');
+	}
+	putchar('\n');
 
-        return (0);
+	return (0);
 }

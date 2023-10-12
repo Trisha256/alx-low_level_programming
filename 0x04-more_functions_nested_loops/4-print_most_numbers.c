fa3c722ae951c525dc151d280cef0b23514f09c9
Patricia Kemirembe <kemirembe2patricia@gmail.com>
2023-10-12 07:57:56 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,12 +9,12 @@
 
 void print_most_numbers(void)
 {
-	char c;
+	int c;
 
 	for (c = '0'; c <= '9'; c++)
 	{
 		if (c != 2 && c != 4)
-				_putchar(c + '0');
+			_putchar(c + '0');
 	}
 	_putchar('\n');
 }

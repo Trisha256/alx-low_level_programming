be0d5c5a0b9f692613c5d0dde0eaf5959938789b
Patricia Kemirembe <kemirembe2patricia@gmail.com>
2023-10-12 08:20:43 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,18 +3,19 @@
 /**
  * print_most_numbers - prints the numbers, from 0 to 9
  * except 2 and 4
- * @c: function to be printed
- * Return: void
+ * Return: the numbers 0 to 9
  */
 
 void print_most_numbers(void)
 {
-	int c;
+	int c = 0;
 
-	for (c = '0'; c <= '9'; c++)
+	for (; c <= 9; c++)
+	if (c == 2 || c == 4)
+		continue;
+	else
 	{
-		if (c != 2 && c != 4)
-			_putchar(c + '0');
+		_putchar(c + '0');
 	}
 	_putchar('\n');
 }

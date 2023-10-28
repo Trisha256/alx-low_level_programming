73c9cc217bd0debdb22cf28f402b52eabe2bbfb9
Patricia Kemirembe <kemirembe2patricia@gmail.com>
2023-10-28 11:27:59 -0700

    4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - a program that adds positive numbers
+ * @argc: argument count
+ * @argv: array of arguments
+ * @sum: addittion of digits
+ * Return 0 (success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, j; int sum = 0;
+
+	if (argc < 1)
+		printf("0\n");
+	for (i = 1; i < argc; i++)
+	{
+	for (j = 0; argv[i][j] != '\0'; j++)
+	{
+		if (!isdigit(argv[i][j]))
+		{
+		printf("Error\n");
+		return (1);
+		}
+	}
+	sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}

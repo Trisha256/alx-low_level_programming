c12e208d5c86b072a6a31dd489773b7bb1f11a0f
Patricia Kemirembe <kemirembe2patricia@gmail.com>
2023-10-31 03:35:48 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,10 +16,11 @@ char *str_concat(char *s1, char *s2)
 	size_t size1 = strlen(s1);
 	size_t size2 = strlen(s2);
 
-	if (s1 == NULL)
-		return (" ");
-	if (s2 == NULL)
-		return (" ");
+	if (s1 == NULL || s2 == NULL)
+	{
+		s1 = (s1 == NULL)? "" :s1;
+		s2 = (s2 == NULL)? "" :s2;
+	}
 	result = (char *)malloc(size1 + size2 + 1);
 	if (result == NULL)
 		return (NULL);

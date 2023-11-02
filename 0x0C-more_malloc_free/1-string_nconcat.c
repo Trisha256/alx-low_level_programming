2c4a8f28d270485d83bdc278921248e04f941f58
Patricia Kemirembe <kemirembe2patricia@gmail.com>
2023-11-02 09:21:52 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,7 +11,7 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *result;
+	char *R;
 	size_t s1_len = strlen(s1);
 	size_t s2_len = strlen(s2);
 	size_t result_len;
@@ -23,12 +23,15 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (n >= s2_len)
 		n = s2_len;
 	result_len = s1_len + n;
-	result = (char *)malloc((result_len + 1) * sizeof(char));
-	if (result == NULL)
+	R = (char *)malloc((result_len + 1) * sizeof(char));
+	if (R == NULL)
 		return (NULL);
-	strncpy(result, s1, s1_len);
-	strncpy(result + s1_len, s2, n);
-	result[result_len] = '\0';
-
-	return (result);
+	strncpy(R, s1, s1_len);
+	strncpy(R + s1_len, s2, n);
+	R[result_len] = '\0';
+	return (R);
 }
+	
+
+
+

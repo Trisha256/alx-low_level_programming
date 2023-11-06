83f3a7a8355af7dafe132e60048c9fb14f376b83
Patricia Kemirembe <kemirembe2patricia@gmail.com>
2023-11-06 10:19:00 -0800

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog basic info
+ * @name: first member
+ * @age: second member
+ * @owner: third member
+ *
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+#endif

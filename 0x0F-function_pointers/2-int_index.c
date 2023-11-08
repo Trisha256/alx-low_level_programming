3a5616f7cf8338483c20043701030439e4ad44c6
Patricia Kemirembe <kemirembe2patricia@gmail.com>
2023-11-08 03:32:04 -0800

    2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -11,17 +11,19 @@
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int i;
+	int i = 0;
 
-	if (size <= 0)
+	if (size >= 0)
 	{
-		return (-1);
-	}
-	for (i = 0; i < size; i++)
-	{
-		if (cmp(array[i]) != 0)
-			return (i);
+		if (array != NULL && cmp != NULL)
+		{
+			while (i < size)
+			{
+				if (cmp(array[i]) != 0)
+					return (i);
+				i++;
+			}
+		}
 	}
 	return (-1);
 }
-

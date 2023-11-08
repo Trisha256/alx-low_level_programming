7a6bae9426b50a3051d4585646dd968d95290552
Patricia Kemirembe <kemirembe2patricia@gmail.com>
2023-11-08 03:47:54 -0800

    1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -12,10 +12,13 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	size_t i;
+	unsigned int i;
 
-	for (i = 0; i < size; i++)
+	if (array != NULL && action != NULL && size > 0)
+	{
+		for (i = 0; i < size; i++)
 	{
 		action(array[i]);
+	}
 	}
 }

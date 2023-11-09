2525a99969a2be05f6f2b28f8cb97693827f6ff6
Patricia Kemirembe <kemirembe2patricia@gmail.com>
2023-11-09 04:41:11 -0800

    3-print_all.c

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,16 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_numbers - a function that prints numbers, followed by a new line.
+ * @separator: the string to be printed btn numbers
+ * @n: number of integers to be passed
+ *
+ * Return: void
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,6 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
+#include <stdarg.h>
 
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
@@ -7,4 +8,16 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
+/**
+ * struct print_type - .....
+ * @f: ...
+ * @type: ..
+ *
+ */
+
+typedef struct print_type
+{
+	char *type;
+	void (*f)(va_list ap);
+} print_type;
 #endif

5a0b7a9ee5300ee7bc844d3c543a847c902019da
Patricia Kemirembe <kemirembe2patricia@gmail.com>
2023-11-22 08:55:29 -0800

    9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -34,6 +34,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (_newnode);
 	}
 
+	current = *head;
 	while (current != NULL && count < idx - 1)
 	{
 		current = current->next;

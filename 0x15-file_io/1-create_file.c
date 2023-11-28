7fb9b8c0e15806a41e03f9b77f83f193223ee087
Patricia Kemirembe <kemirembe2patricia@gmail.com>
2023-11-27 23:50:11 -0800

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,22 +11,25 @@
 int create_file(const char *filename, char *text_content)
 {
 	int _fd;
-	int letters;
-	int _rw;
+	ssize_t _result;
 
 	if (!filename)
+	{
 		return (-1);
+	}
 
-	_fd = open(filename | O_WRONLY | O_CREAT | O_TRUNC | 0600);
+	_fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (_fd == -1)
 		return (-1);
-	if (!text_content)
-		text_content = "";
-	for (letters = 0; text_content[letters]; letters++)
-		;
-	_rw = write(_fd, text_content, letters);
-		if (_rw == -1)
-			return (-1);
+	if (text_content != NULL)
+	{
+	_result = write(_fd, text_content, strlen(text_content));
+	if (_result == -1)
+	{
+		close(_fd);
+		return (-1);
+	}
+	}
 	close(_fd);
 	return (1);
 }
